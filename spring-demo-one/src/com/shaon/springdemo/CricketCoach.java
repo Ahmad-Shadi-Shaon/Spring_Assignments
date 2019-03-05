2be8b190f956e5f1986a3e23a427d68918733e45
Shaon77 <ahmadshaon067@gmail.com>
2019-03-05 18:59:34 +0600

    assignment 1.2 is done

--- spring-demo-one/src/com/shaon/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/shaon/springdemo/CricketCoach.java
@@ -1,11 +1,24 @@
 package com.shaon.springdemo;
 
 public class CricketCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public CricketCoach(FortuneService theFortuneService)
+	{
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		
 		return "Do 30 mins workout";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
 	}
 
 }
